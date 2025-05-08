d249e07eca28ebeaec23f547db3605a0afea161c
Eugene Fox <eugene@xfox111.net>
2025-05-08 09:04:08 +0300

    fix: corrupted json export

--- entrypoints/options/utils/exportData.ts
+++ entrypoints/options/utils/exportData.ts
@@ -4,13 +4,16 @@ export default async function exportData(): Promise<void>
 		local: await browser.storage.local.get(null),
 		sync: await browser.storage.sync.get(null)
 	});
+	const blob: Blob = new Blob([data], { type: "application/json" });
 
 	const element: HTMLAnchorElement = document.createElement("a");
 	element.style.display = "none";
-	element.href = `data:application/json;charset=utf-8,${data}`;
+	element.href = URL.createObjectURL(blob);
 	element.setAttribute("download", "tabs-aside_data.json");
 
 	document.body.appendChild(element);
 	element.click();
+
+	URL.revokeObjectURL(element.href);
 	document.body.removeChild(element);
 };
