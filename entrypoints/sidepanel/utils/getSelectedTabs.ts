53adbd4f75756efabb826792abf87a6a6549093f
Eugene Fox <eugene@xfox111.net>
2025-07-11 13:53:27 +0300

    fix: adding forbidden tabs via "Add selected tabs"

--- entrypoints/sidepanel/utils/getSelectedTabs.ts
+++ entrypoints/sidepanel/utils/getSelectedTabs.ts
@@ -1,8 +1,24 @@
 import { TabItem } from "@/models/CollectionModels";
+import sendNotification from "@/utils/sendNotification";
 import { Tabs } from "wxt/browser";
 
 export default async function getSelectedTabs(): Promise<TabItem[]>
 {
-	const tabs: Tabs.Tab[] = await browser.tabs.query({ currentWindow: true, highlighted: true });
-	return tabs.filter(i => i.url).map(i => ({ type: "tab", url: i.url!, title: i.title }));
+	let tabs: Tabs.Tab[] = await browser.tabs.query({ currentWindow: true, highlighted: true });
+	const tabCount: number = tabs.length;
+
+	tabs = tabs.filter(i =>
+		i.url
+		&& new URL(i.url).protocol !== "about:"
+		&& new URL(i.url).hostname !== "newtab"
+	);
+
+	if (tabs.length < tabCount)
+		await sendNotification({
+			title: i18n.t("notifications.partial_save.title"),
+			message: i18n.t("notifications.partial_save.message"),
+			icon: "/notification_icons/save_warning.png"
+		});
+
+	return tabs.map(i => ({ type: "tab", url: i.url!, title: i.title }));
 }
